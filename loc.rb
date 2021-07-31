b1b624f3a1aef3ad6ba9c99b5310fa09d4b4c96b
Tammy Metz <tammy@tammymetz.com>
2021-07-31 16:52:14 +0000

    add error handling...sort of

--- loc.rb
+++ loc.rb
@@ -1,14 +1,31 @@
 require 'faraday'
+require 'json'
 
 
 conn = Faraday.new(
-  url: 'https://www.loc.gov/collections',
-  params: {fo:'json', at:'results'},
+  url: 'https://www.loc.gov/item/2014717546',
+  #url: 'https://www.loc.gov/item/2014717546d',
+  params: {fo:'json'},
   headers: {'Content-Type' => 'application/json'}
 )
 
 response = conn.get
 
+body_hash = JSON.parse(response.body)
+
+if response.success?
+    @citations = body_hash["cite_this"]
+else
+    #error = body["meta"]["errorDetail"]
+    error = "that did not work"
+end
+
+
+
 puts response.status
 
 puts response.headers
+
+puts @citations
+
+puts error
